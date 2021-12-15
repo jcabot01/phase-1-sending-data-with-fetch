8aa2fb63d8a4108b9efbb5bef09ce59b3c274f1c
JonCabot <jfc0053@gmail.com>
2021-12-14 22:03:06 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+
+function submitData(firstName, email) {
+    const contactObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email,
+        })
+    };
+    return fetch("http://localhost:3000/users", contactObj)
+    .then(res => res.json())
+    .then(body => addNewContact(body.id))
+    .catch(errorMessage) 
+}
+
+function addNewContact(id) {
+    document.getElementsByTagName('body')[0].innerHTML = id;
+}
+
+function errorMessage(message) {
+    document.getElementsByTagName('body')[0].innerHTML = message;
+}    
+ 
